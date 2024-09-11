381a7d486715e7b2bba0b88142e5c91dc3af346d
Francisco Iglesias Saldias <culini13@hotmail.com>
2024-09-11 23:56:53 +0200

    comenzando con responsive basico

--- carousel/carouselScript.js
+++ carousel/carouselScript.js
@@ -24,6 +24,10 @@ const shelterCarouselImg = ['url(./pictures/preFabric/preFabricadaBackClose.jpg)
 const domesCarouselImg = ['url(./pictures/domos/domoChillingCordillera.jpg)', 'url(./pictures/domos/domoEvent.jpg)', 'url(./pictures/domos/domoHabitacionSalamandra.jpg)'];
 const bricksCarouselImg = ['url(./pictures/bricks/bricksDuplex.jpg)', 'url(./pictures/bricks/bricksHouse.jpg)', 'url(./pictures/bricks/bricksHouse1.jpg)'];
 /* estructura Carousel */
+const textNosotrosMovile = ['<h1>Nosotros</h1>', '<h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>'];
+const textShelterMovile = ['<h1>Shelter</h1>', '<h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>'];
+const textBricksMovile = ['<h1>Bricks</h1>', '<h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>'];
+const textDomesMovile = ['<h1>Domes</h1>', '<h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>', '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>'];
 const textNosotros = [`<h1>Nosotros</h1>
         <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`, `<h1>Nosotros 2</h1>
@@ -58,7 +62,7 @@ let shelterIndex = 0;
 let domesIndex = 0;
 let bricksIndex = 0;
 /* contenido inicial */
-nosotrosContent.innerHTML = `<h1>Nosotros</h1>
+/* nosotrosContent.innerHTML = `<h1>Nosotros</h1>
         <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`
 shelterContent.innerHTML = `<h1>Shelter</h1>
@@ -69,50 +73,104 @@ domesContent.innerHTML = `<h1>Domes</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`;
 bricksContent.innerHTML = `<h1>Bricks</h1>
         <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`;
-const changeBackgroundRight = (i, image, contenedor, contentCar, textContent) => {
-    i += 1;
-    if (i < 3) {
-        console.log(i);
-        contenedor.style.backgroundImage = image[i];
-        let contenido = textContent[i];
-        contentCar.innerHTML = `${contenido}`;
-        /* document.contentCar.appendchild(content); */
-    }
-    else {
-        i = 0;
-        contenedor.style.backgroundImage = image[i];
-        let contenido = textContent[i]
-        contentCar.innerHTML = `${contenido}`;
-        /* document.contentCar.appendchild(contentCar); */
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`; */
+/* Responsive */
+function myFunction(x) {
+        if (x.matches) { // If media query matches
+                nosotrosContent.innerHTML = `<h1>Nosotros</h1>`;
+                shelterContent.innerHTML = `<h1>Shelter</h1>`;
+                domesContent.innerHTML = `<h1>Domes</h1>`;
+                bricksContent.innerHTML = `<h1>Bricks</h1>`;
+                
+                /* Nosotros */
+                nosotrosButtonRight.addEventListener("click", () => { nosotrosIndex = changeBackgroundRight(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotrosMovile) });
+                nosotrosButtonLeft.addEventListener("click", () => { nosotrosIndex = changeBackgroundLeft(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotrosMovile) });
 
-    }
-    return i;
+                /* Shelter */
+                shelterButtonRight.addEventListener("click", () => { shelterIndex = changeBackgroundRight(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent, textShelterMovile) });
+                shelterButtonLeft.addEventListener("click", () => { shelterIndex = changeBackgroundLeft(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent, textShelterMovile) });
+
+                /* Domes */
+                domesButtonRight.addEventListener("click", () => { domesIndex = changeBackgroundRight(domesIndex, domesCarouselImg, domesCarousel, domesContent, textDomesMovile) });
+                domesButtonLeft.addEventListener("click", () => { domesIndex = changeBackgroundLeft(domesIndex, domesCarouselImg, domesCarousel, domesContent, textDomesMovile) });
+
+                /* Bricks */
+                bricksButtonRight.addEventListener("click", () => { bricksIndex = changeBackgroundRight(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent, textBricksMovile) });
+                bricksButtonLeft.addEventListener("click", () => { bricksIndex = changeBackgroundLeft(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent, textBricksMovile) });
+        } else {
+                /* Nosotros */
+                nosotrosButtonRight.addEventListener("click", () => { nosotrosIndex = changeBackgroundRight(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotros) });
+                nosotrosButtonLeft.addEventListener("click", () => { nosotrosIndex = changeBackgroundLeft(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotros) });
+
+                /* Shelter */
+                shelterButtonRight.addEventListener("click", () => { shelterIndex = changeBackgroundRight(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent, textShelter) });
+                shelterButtonLeft.addEventListener("click", () => { shelterIndex = changeBackgroundLeft(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent, textShelter) });
+
+                /* Domes */
+                domesButtonRight.addEventListener("click", () => { domesIndex = changeBackgroundRight(domesIndex, domesCarouselImg, domesCarousel, domesContent, textDomes) });
+                domesButtonLeft.addEventListener("click", () => { domesIndex = changeBackgroundLeft(domesIndex, domesCarouselImg, domesCarousel, domesContent, textDomes) });
+
+                /* Bricks */
+                bricksButtonRight.addEventListener("click", () => { bricksIndex = changeBackgroundRight(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent, textBricks) });
+                bricksButtonLeft.addEventListener("click", () => { bricksIndex = changeBackgroundLeft(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent, textBricks) });
+
+                nosotrosContent.innerHTML = `<h1>Nosotros</h1>
+                <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`
+                shelterContent.innerHTML = `<h1>Shelter</h1>
+                <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`;
+                domesContent.innerHTML = `<h1>Domes</h1>
+                <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`;
+                bricksContent.innerHTML = `<h1>Bricks</h1>
+                <h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias dolorum minima sapiente exercitationem eaque, ab ea harum tempora quae temporibus sed voluptas, odio magnam aliquid accusamus hic, nulla ipsum.</p>`;
+        }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 768px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+        myFunction(x);
+});
+const changeBackgroundRight = (i, image, contenedor, contentCar, textContent) => {
+        i += 1;
+        if (i < 3) {
+                console.log(i);
+                contenedor.style.backgroundImage = image[i];
+                let contenido = textContent[i];
+                contentCar.innerHTML = `${contenido}`;
+                /* document.contentCar.appendchild(content); */
+        }
+        else {
+                i = 0;
+                contenedor.style.backgroundImage = image[i];
+                let contenido = textContent[i]
+                contentCar.innerHTML = `${contenido}`;
+                /* document.contentCar.appendchild(contentCar); */
+
+        }
+        return i;
 }
 const changeBackgroundLeft = (i, image, contenedor, contentCar, textContent) => {
-    i -= 1;
-    if (i < 0) {
-        i = 2;
-        contenedor.style.backgroundImage = image[i];
-        let contenido = textContent[i]
-        contentCar.innerHTML = `${contenido}`;
-    }
-    else {
-        contenedor.style.backgroundImage = image[i];
-        let contenido = textContent[i]
-        contentCar.innerHTML = `${contenido}`;
-    }
-    return i;
+        i -= 1;
+        if (i < 0) {
+                i = 2;
+                contenedor.style.backgroundImage = image[i];
+                let contenido = textContent[i]
+                contentCar.innerHTML = `${contenido}`;
+        }
+        else {
+                contenedor.style.backgroundImage = image[i];
+                let contenido = textContent[i]
+                contentCar.innerHTML = `${contenido}`;
+        }
+        return i;
 }
-/* Nosotros */
-nosotrosButtonRight.addEventListener("click", () => { nosotrosIndex = changeBackgroundRight(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotros) });
-nosotrosButtonLeft.addEventListener("click", () => { nosotrosIndex = changeBackgroundLeft(nosotrosIndex, nosotrosCarouselImg, nosotrosCarousel, nosotrosContent, textNosotros) });
-/* Shelter */
-shelterButtonRight.addEventListener("click", () => { shelterIndex = changeBackgroundRight(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent,textShelter) });
-shelterButtonLeft.addEventListener("click", () => { shelterIndex = changeBackgroundLeft(shelterIndex, shelterCarouselImg, shelterCarousel, shelterContent, textShelter) });
-/* Domes */
-domesButtonRight.addEventListener("click", () => { domesIndex = changeBackgroundRight(domesIndex, domesCarouselImg, domesCarousel, domesContent,textDomes) });
-domesButtonLeft.addEventListener("click", () => { domesIndex = changeBackgroundLeft(domesIndex, domesCarouselImg, domesCarousel, domesContent, textDomes) });
-/* Bricks */
-bricksButtonRight.addEventListener("click", () => { bricksIndex = changeBackgroundRight(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent,textBricks) });
-bricksButtonLeft.addEventListener("click", () => { bricksIndex = changeBackgroundLeft(bricksIndex, bricksCarouselImg, bricksCarousel, bricksContent, textBricks) });
+
---
+++ navBar/navBar.js
@@ -0,0 +1,84 @@
+const navButton = document.querySelector("#navButton");
+const options = document.querySelector("#options");
+const lista = document.querySelector("#lista");
+const navBar = document.querySelector("#navBar");
+let count = 0;
+let start = 0;
+const collapsedMenu = () => {
+    if(start == 0)
+    {
+        if (count == 0) {
+            lista.style.display = 'flex';
+            lista.style.position = 'fixed';
+            navBar.style.flexWrap = 'wrap';
+            options.style.position = 'relative'
+    
+            count++;
+        } else {
+            lista.style.display = 'none';
+            count = 0;
+    
+        }
+        start ++;
+    }
+    else
+    {
+        if (count == 0) {
+            lista.style.display = 'flex';
+            lista.style.position = 'fixed';
+            navBar.style.flexWrap = 'wrap';
+            options.style.position = 'relative'
+            count++;
+        } else {
+            lista.style.display = 'none';
+            count = 0;
+        }
+    }
+}
+const scrollNavBar = () => {
+    if(start == 0)
+    {
+        lista.style.display = 'flex';
+        count = 0;
+    }
+}
+
+
+navButton.addEventListener("click", collapsedMenu);
+window.addEventListener("scroll", scrollNavBar);
+
+/* function myFunction(x) {
+    if (x.matches) { // If media query matches
+        #navButton{
+                 width: 25vw;
+                height: 15vh; 
+            width: 25vw;
+            display: block;
+            background - color: black;
+            color: aliceblue;
+            font - size: 30px;
+            opacity: 0.6;
+            right: 5px;
+        }
+    } else {
+        navButton.style.display = 'none';
+        lista.style.display = 'flex';
+        lista.style.flexDirection = 'row';
+        lista.style.width = '60vw';
+        lista.style.height = '25vh';
+        options.style.width = '60vw';
+        navBar.style.flexWrap = 'wrap';
+        options.style.position = 'relative';
+    }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 768px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+    myFunction(x);
+}); */
